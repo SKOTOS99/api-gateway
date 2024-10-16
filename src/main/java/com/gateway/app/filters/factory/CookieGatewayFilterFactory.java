89aceb2376d0cb5b5f3dd2e76798fb79b2094e12
SKOTOS99 <felipd95@hotmail.com>
2024-10-15 18:24:27 -0600

    add model cookie and apply to apis

--- src/main/java/com/gateway/app/filters/factory/CookieGatewayFilterFactory.java
+++ src/main/java/com/gateway/app/filters/factory/CookieGatewayFilterFactory.java
@@ -1,9 +1,15 @@
 package com.gateway.app.filters.factory;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
+import com.gateway.app.filters.GlobalFilters;
 import com.gateway.app.models.cookie.ConfigurationCookie;
 
 import reactor.core.publisher.Mono;
@@ -11,7 +17,8 @@ import reactor.core.publisher.Mono;
 @Component
 public class CookieGatewayFilterFactory extends AbstractGatewayFilterFactory<ConfigurationCookie>{
 
-	
+	private final Logger log = LoggerFactory.getLogger(CookieGatewayFilterFactory.class);
+
 	public CookieGatewayFilterFactory() {
 		super(ConfigurationCookie.class);
 	}
@@ -20,10 +27,14 @@ public class CookieGatewayFilterFactory extends AbstractGatewayFilterFactory<Con
 	public GatewayFilter apply(ConfigurationCookie config) {
 		return (exchange, chain) ->{
 			
-			
+			log.info("ejecutando filter pre factory: {}",  config.getMessage());
 			return chain.filter(exchange).then(Mono.fromRunnable(() ->{
+				log.info("ejecutando filter pre factory: {}",  config.getMessage());
 				
-				
+				Optional.ofNullable(config.getValue()).ifPresent(cookie ->{
+					exchange.getResponse().addCookie(ResponseCookie.from(config.getName(), cookie).build());
+				});
+
 			}));
 		};
 	}
